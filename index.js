2b477304bae3997cdb4d2bd17707578efcc22140
Christina Duncan <heyaquamark@gmail.com>
2025-06-04 09:37:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { createClient } = require("@supabase/supabase-js");
 const QRCode = require("qrcode");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
